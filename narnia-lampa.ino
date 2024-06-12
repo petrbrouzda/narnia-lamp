97d32673d8aaeca752cfdb30f44303b319d246db
PB <brouzda@datasys.cz>
2024-06-12 13:37:47 +0200

    webserver funguje

---
+++ narnia-lampa.ino
@@ -0,0 +1,192 @@
+/*
+Potrebuje:
+https://github.com/me-no-dev/ESPAsyncWebServer/archive/master.zip
+https://github.com/me-no-dev/AsyncTCP/archive/master.zip
+
+dokumentace k webservu zde:
+https://github.com/me-no-dev/ESPAsyncWebServer/tree/master
+
+*/
+
+/**
+Nastaveni kompilace
+*/
+
+
+/**
+Ovládání:
+
+1) V mobilu vypnout data, zapnout wifi, připojit na síť
+    MORNING*STAR
+  s heslem
+    zlutalampa
+
+2) V prohlížeči 
+  http://192.168.1.1
+  a zobrazí se informace o stavu
+
+*/
+
+
+// Import required libraries
+
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+#include <WiFi.h>
+
+#include <SPI.h>
+#include <Wire.h>
+
+/* Konfiguracni soubor na SPIFFS filesystemu. Urcite se to da resit jednoduseji, ale tohle mam hotove z jineho projeku. */
+#include "ConfigProvider.h"
+#include "src/ra/raConfig.h"
+#include "src/ra/raLogger.h"
+raLogger* logger;
+raConfig config;
+#define RA_CONFIG_PASSPHRASE "--------"
+
+/*
+#include <ESP32AnalogRead.h>
+ESP32AnalogRead adc4;
+*/
+
+#include "Tasker.h"
+Tasker tasker;
+
+
+// Replace with your network credentials
+const char* ssid = "MORNING*STAR";
+const char* password = "zlutalampa";
+
+/* Put IP Address details */
+IPAddress local_ip(192,168,1,1);
+IPAddress gateway(192,168,1,1);
+IPAddress subnet(255,255,255,0);
+
+
+#include <Adafruit_NeoPixel.h>
+
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN        14 
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS 4 
+
+// When setting up the NeoPixel library, we tell it how many pixels,
+// and which pin to use to send signals. Note that for older NeoPixel
+// strips you might need to change the third parameter -- see the
+// strandtest example for more information on possible values.
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+
+/**
+1 = ohen
+2 = morzeovka
+3 = konstantni barva
+4 = blikani
+*/
+int aktualniRezim = 1;
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+
+void OnWiFiEvent(WiFiEvent_t event)
+{
+  switch (event) {
+ 
+    case SYSTEM_EVENT_STA_CONNECTED:
+      Serial.println("ESP32 Connected to WiFi Network");
+      break;
+    case SYSTEM_EVENT_AP_START:
+      Serial.println("ESP32 soft AP started");
+      break;
+    case SYSTEM_EVENT_AP_STACONNECTED:
+      Serial.println("Station connected to ESP32 soft AP");
+      break;
+    case SYSTEM_EVENT_AP_STADISCONNECTED:
+      Serial.println("Station disconnected from ESP32 soft AP");
+      break;
+    case SYSTEM_EVENT_AP_STAIPASSIGNED:
+      Serial.println("Station got IP");
+      break;
+    default: break;
+  }
+}
+
+
+// provozni hodnoty
+
+
+/** nacte konfiguraci ze souboru */
+void loadConfigData() {
+  
+  // const char * p = config.getString( "limitBaterka", "12.3" ); 
+  // limitBaterka = atof( p );
+  // Serial.printf( "limitBaterka = %f\n", limitBaterka);
+
+  aktualniRezim = (int) config.getLong( "rezim", 1 );
+  
+}
+
+/** ulozi konfiguraci do souboru */
+void saveConfigData() {
+  char buf[20];
+
+  sprintf( buf, "%d", aktualniRezim );
+  config.setValue( "rezim", buf );
+
+  saveConfig( &config );
+}
+
+
+
+
+
+void setup(){
+  Serial.begin(115200);
+  delay(500);
+  Serial.println( "Startuju" );
+
+  logger = new raLogger( RA_LOG_MODE_SERIAL );
+  logger->log( "raLogger started");
+
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  
+  config.setInfo( (char*)"-", (char*)RA_CONFIG_PASSPHRASE );
+  loadConfig( &config );
+  loadConfigData();
+
+  // adc4.attach(4);
+
+  //WiFi.mode(WIFI_OFF);
+  delay(1000);
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+  delay(100);
+  WiFi.softAP(ssid, password, 1, false );
+  Serial.println("Wait 200 ms for AP_START...");
+  delay(500);
+  WiFi.softAPsetHostname(ssid);
+
+  
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+
+  WiFi.softAPsetHostname(ssid);
+
+  WiFi.onEvent(OnWiFiEvent);
+
+  webserverBegin();
+
+  // tasker.setInterval(vypisStav, 5000);
+}
+
+
+void loop() {
+  tasker.loop();
+}
+
+/*
+*/
---
+++ webserver.ino
@@ -0,0 +1,116 @@
+
+const char hlavicka[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <title>Narnia lampa</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    html {font-family: Arial; display: inline-block; text-align: left;}
+    h2 {font-size: 2.0rem;}
+    p {font-size: 1.5rem;}
+    body {max-width: 600px; margin:0px auto; padding-bottom: 25px;}
+  </style>
+</head>
+<body>
+)rawliteral";
+
+const char paticka[] PROGMEM = R"rawliteral(
+</body>
+</html>
+)rawliteral";
+
+
+const char* CONTENT_TYPE = "text/html; charset=utf-8";
+
+void onRequestStatus(AsyncWebServerRequest *request){
+  logger->log( "status");
+
+  //Handle Unknown Request
+  AsyncResponseStream *response = request->beginResponseStream(CONTENT_TYPE);
+  response->print( hlavicka );
+  response->print("<h2>Narnia lampa</h2>");
+  response->printf( "Rezim: %d", aktualniRezim );
+
+  response->print("<form method=\"GET\" action=\"/rezim1\">");
+  response->print("<input type=\"submit\" name=\"nastav\" value=\"režim 1\" >" );
+  response->print("</form>" );
+
+  response->print("<form method=\"GET\" action=\"/rezim2\">");
+  response->print("<input type=\"submit\" name=\"nastav\" value=\"režim 2\" >" );
+  response->print("</form>" );
+
+  response->printf( "<br><i>Lokalni cas (sec od bootu): %d</i><br>", time(NULL) );
+
+  response->print( paticka );
+  request->send(response);
+}
+
+void onRequestRezim1(AsyncWebServerRequest *request){
+  logger->log( "* rezim 1");
+
+  aktualniRezim = 1;
+  request->redirect("/");
+}
+
+void onRequestRezim2(AsyncWebServerRequest *request){
+  logger->log( "* rezim 2");
+
+  aktualniRezim = 2;
+  request->redirect("/");
+}
+
+void webserverBegin() 
+{
+  server.begin();
+
+  server.on("/", HTTP_GET, onRequestStatus );
+  server.on("/rezim1", HTTP_GET, onRequestRezim1 );
+  server.on("/rezim2", HTTP_GET, onRequestRezim2 );
+}
+
+/*
+  // Send a GET request to <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
+  server.on("/updateVal", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String inputMessage1;
+    // GET input1 value on <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
+
+    if(request->hasParam("V5") ) {
+      inputMessage1 = request->getParam("V5")->value();
+      if( strcmp(inputMessage1.c_str(),"TotalHeslo" )==0 ) {
+
+        if(request->hasParam("V1") ) {
+          inputMessage1 = request->getParam("V1")->value();
+          limitBaterka = atof(inputMessage1.c_str() );
+        }
+
+        if(request->hasParam("V6") ) {
+          inputMessage1 = request->getParam("V6")->value();
+          varovaniBaterka = atof(inputMessage1.c_str() );
+        }
+    
+        if(request->hasParam("V2") ) {
+          inputMessage1 = request->getParam("V2")->value();
+          delkaSpusteni = atol(inputMessage1.c_str() );
+          if( delkaSpusteni>900 ) delkaSpusteni = 900;
+        }
+        if(request->hasParam("V3") ) {
+          inputMessage1 = request->getParam("V3")->value();
+          delkaPauzy = atol(inputMessage1.c_str() );
+        }
+        if(request->hasParam("V4") ) {
+          inputMessage1 = request->getParam("V4")->value();
+          pocetSpusteni = atol(inputMessage1.c_str() );
+        }
+
+        saveConfigData();
+        loadConfigData();
+
+        request->redirect("/nastav");
+      } else { 
+        request->redirect("/");
+      }
+    } else {
+      request->redirect("/");
+    } 
+  });
+  */
